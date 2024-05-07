90462dd61741bdf38991d9e5bec6c7b08e58eec3
Overpowering-Victorious <101652744+Overpowering-Victorious@users.noreply.github.com>
2024-05-07 16:39:10 +0530

    Update scene3.js

--- scene3.js
+++ scene3.js
@@ -7,7 +7,7 @@ class scene3 extends Phaser.Scene
 
     create()
     {
-        this.add.text(config.width/2,config.height/2,"Game Over !!",{ fontSize: '40px', align: 'center', fill: '#fff' });
+        this.add.text(config.width/2-150,config.height/2,"Game Over !!",{ fontSize: '40px', align: 'center', fill: '#fff' });
         this.gameover=this.sound.add("gameover");
         this.gameover.play();
         this.input.on('pointerdown', () => {
